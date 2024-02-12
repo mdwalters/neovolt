fdc1641dc7cdddb2a6d6701951d4f123b2ad0c25
Max Walters <mdwalters.pm@proton.me>
2024-02-12 22:56:54 +0000

    fix: change link colour

--- src/home.rs
+++ src/home.rs
@@ -8,7 +8,7 @@ pub fn Home() -> Html {
                 <h1>{ "Neovolt" }</h1>
                 <p>{ "The cutest Revolt client of the yet" }</p>
                 <br />
-                <small>{ "Join the Revolt server: " } <b><a href="https://rvlt.gg/Qzmc4b5p">{ "https://rvlt.gg/Qzmc4b5p" }</a></b></small>
+                <small>{ "Join the Revolt server: " } <b><a href="https://rvlt.gg/Qzmc4b5p">{ "rvlt.gg/Qzmc4b5p" }</a></b></small>
             </div>
         </>
     }
