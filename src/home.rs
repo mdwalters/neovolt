c476abef91847157e657986ef5626c1a91e5d971
Max Walters <mdwalters.pm@proton.me>
2024-02-12 22:52:54 +0000

    chore: add revolt server link

--- src/home.rs
+++ src/home.rs
@@ -7,6 +7,8 @@ pub fn Home() -> Html {
             <div class="container">
                 <h1>{ "Neovolt" }</h1>
                 <p>{ "The cutest Revolt client of the yet" }</p>
+                <br />
+                <small>{ "Join the Revolt server: " } <b><a href="https://rvlt.gg/Qzmc4b5p">{ "https://rvlt.gg/Qzmc4b5p" }</a></b></small>
             </div>
         </>
     }
