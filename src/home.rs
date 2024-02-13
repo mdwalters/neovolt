04dc130adac3d06351b133ccf424da95d08cd6c5
Max Walters <mdwalters.pm@proton.me>
2024-02-13 04:29:31 +0000

    chore: dino :3
    Currently unused

--- src/home.rs
+++ src/home.rs
@@ -6,9 +6,18 @@ pub fn Home() -> Html {
         <>
             <div class="modal-container">
                 <div class="modal">
+                    // is it even legal to use this guy as a mascot
+                    // i should really try to get someone to make a mascot for this
+                    //
+                    // <img
+                    //    class="hero-emoji"
+                    //    src="https://fonts.gstatic.com/s/e/notoemoji/latest/1f995/512.gif"
+                    //    alt="🦕"
+                    // />
+
                     <h1>{ "Neovolt" }</h1>
                     <p>{ "The cutest Revolt client of the yet" }</p>
-                    
+
                     <small class="bottom">
                         { "Join the Revolt server: " }
                         <a href="https://rvlt.gg/Qzmc4b5p">{ "rvlt.gg/Qzmc4b5p" }</a>
