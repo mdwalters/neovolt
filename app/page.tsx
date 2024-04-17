c661cb1f7006a8ec4321bcbff6d2a9958c1cc67d
theycallhermax <mdwalters.pm@proton.me>
2024-04-16 22:57:17 -0400

    port to next.js

---
+++ app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+    return (
+        <>
+            <h1>Neovolt</h1>
+            <p>The Revolt client of the yet</p>
+    );
+}
