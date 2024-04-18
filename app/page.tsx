210008cca02ccfad4ecaffdc8a14d105fbcbfd77
theycallhermax <mdwalters.pm@proton.me>
2024-04-18 07:44:51 -0400

    add revolt.js

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,19 @@
+import { Client } from "revolt.js";
 import Image from "next/image";
 
-export default function Home() {
+const revolt: Client = new Client();
+
+// temporaily here for debugging purposes
+const email: string = "";
+const password: string = "";
+
+export default async function Home() {
+    await revolt.login({
+        "friendly_name": "Neovolt",
+        "email": email,
+        "password": password
+    });
+
     return (
         <>
             <h1>Neovolt</h1>
